b32cd97e5c63449f18bfbebdbbccc1c6b6007c64
Yash Kumar Verma <yashkumarverma623@gmail.com>
2025-05-24 12:30:16 +0530

    Update server.js CORSpolicy

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // CORS configuration
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://127.0.0.1:5500'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:5500','https://games-grid.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
